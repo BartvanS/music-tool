f83cfb9ff3187db9d084d45af546b67c99d54d41
Bart van Sevenbergen <bartvansevenbergen@hotmail.com>
2020-11-04 21:26:45 +0100

    select song and repeat in time frame

---
+++ song_playing_tool/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using song_playing_tool.classes;
+
+namespace song_playing_tool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MusicPlayer player;
+        public MainWindow()
+        {
+            player = new MusicPlayer();
+        }
+
+       
+
+        private void SelectSongBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (player.SearchSong())
+            {
+            }
+        }
+
+        private void StartStopBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (player.soundPlayer.HasAudio)
+            {
+                if (Convert.ToInt32(StopTimeInput.Text) < Convert.ToInt32(StartTimeInput.Text))
+                {
+                    throw new ArgumentOutOfRangeException("Stop time has to be bigger than starttime");
+                }
+                TimeSpan startTime = StartTimeInput.Text.Length > 0 ? TimeSpan.FromSeconds(Convert.ToInt32(StartTimeInput.Text)) : TimeSpan.FromSeconds(0);
+                TimeSpan stopTime = StopTimeInput.Text.Length > 0 ? TimeSpan.FromSeconds(Convert.ToInt32(StopTimeInput.Text)) : TimeSpan.FromSeconds(0);
+                player.PlayPause(startTime, stopTime);
+            }
+            else
+            {
+                MessageBox.Show("No song selected");
+            }
+        }
+
+        private void SongSettingsUpdateBtn_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
---
+++ song_playing_tool/classes/MusicPlayer.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Media;
+// using Microsoft.Win32;
+using System.Windows.Forms;
+using System.Windows.Media;
+
+namespace song_playing_tool.classes
+{
+    public class MusicPlayer
+    {
+        public MediaPlayer soundPlayer;
+        private string filePath = string.Empty;
+        private Timer tmrWmpPlayerPosition;
+        private bool isPlaying = false;
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan StopTime { get; set; }
+        public string SongName { get; set; }
+        public bool HasStarted { get; set; } = false;
+
+        public MusicPlayer()
+        {
+            soundPlayer = new MediaPlayer();
+        }
+
+
+        public bool SearchSong()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "C:\\Users\\bartv\\Music";
+            openFileDialog.Filter = "mp3 files (*.mp3)|*.mp3";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //Get the path of specified file
+                // filePath = "C:\\Users\\bartv\\Music\\wywh.mp3";
+                filePath = openFileDialog.FileName;
+                SongName = openFileDialog.SafeFileName;
+                try
+                {
+                    soundPlayer.Stop();
+                    soundPlayer.Close();
+                    // Load the .mp3 file.
+                    soundPlayer.Open(new Uri(filePath));
+                    if (!HasStarted)
+                    {
+                        StopWmpPlayerTimer();
+                        StartWmpPlayerTimer();
+                    }
+                    else
+                    {
+                        HasStarted = false;
+                    }
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("wubba lubba something something load song: " + ex.Message);
+                }
+            }
+
+            return false;
+        }
+
+        public void PlaySong()
+        {
+            soundPlayer.Play();
+            this.isPlaying = true;
+        }
+
+        public void PauseSong()
+        {
+            soundPlayer.Pause();
+            this.isPlaying = false;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="startTime">time in seconds</param>
+        /// <param name="stopTime">time in seconds</param>
+        public void PlayPause(TimeSpan startTime, TimeSpan stopTime)
+        {
+            if (!HasStarted)
+            {
+                StartTime = startTime;
+                soundPlayer.Position = StartTime;
+                StopTime = stopTime;
+                HasStarted = true;
+            }
+
+            if (!isPlaying)
+            {
+                PlaySong();
+            }
+            else
+            {
+                PauseSong();
+            }
+        }
+
+        private void tmrWmpPlayerPosition_Tick(object sender, EventArgs e)
+        {
+            if (StopTime > soundPlayer.Position) return;
+            soundPlayer.Position = StartTime;
+            // if (notloop....)
+            // {
+                // StopWmpPlayerTimer();
+            // }
+        }
+
+        private void StartWmpPlayerTimer()
+        {
+            tmrWmpPlayerPosition = new Timer();
+            tmrWmpPlayerPosition.Tick += new EventHandler(tmrWmpPlayerPosition_Tick);
+            tmrWmpPlayerPosition.Enabled = true;
+            tmrWmpPlayerPosition.Interval = 500;
+            tmrWmpPlayerPosition.Start();
+        }
+
+        private void StopWmpPlayerTimer()
+        {
+            if (tmrWmpPlayerPosition != null)
+                tmrWmpPlayerPosition.Dispose();
+            tmrWmpPlayerPosition = null;
+        }
+    }
+}
